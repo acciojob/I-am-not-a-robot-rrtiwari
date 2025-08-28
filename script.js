401ff9828790a95ddefa4970a7ea79cf5e77bd3e
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-08-28 10:02:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-let allImages = [
-  "https://picsum.photos/id/1015/150/150",
-  "https://picsum.photos/id/1016/150/150",
-  "https://picsum.photos/id/1018/150/150",
-  "https://picsum.photos/id/1020/150/150",
-  "https://picsum.photos/id/1024/150/150"
-];
-
 let imageContainer = document.getElementById("image-container");
 let resetBtn = document.getElementById("reset");
 let verifyBtn = document.getElementById("verify");
 let message = document.getElementById("para");
 
+let classes = ["img1", "img2", "img3", "img4", "img5"];
 let selected = [];
 
 function start() {
@@ -20,15 +13,13 @@ function start() {
   verifyBtn.style.display = "none";
   selected = [];
 
-  let duplicate = allImages[Math.floor(Math.random() * allImages.length)];
-  let sixImages = [...allImages, duplicate];
-  sixImages.sort(() => Math.random() - 0.5);
+  let duplicate = classes[Math.floor(Math.random() * classes.length)];
+  let sixClasses = [...classes, duplicate];
+  sixClasses.sort(() => Math.random() - 0.5);
 
-  for (let i = 0; i < sixImages.length; i++) {
+  for (let i = 0; i < sixClasses.length; i++) {
     let img = document.createElement("img");
-    img.src = sixImages[i];
-    img.dataset.value = sixImages[i];
-    img.classList.add("img" + (i + 1));   // ✅ add class for Cypress
+    img.classList.add(sixClasses[i]);
     imageContainer.appendChild(img);
   }
 
@@ -37,7 +28,7 @@ function start() {
   images.forEach(img => {
     img.addEventListener("click", () => {
       if (selected.length < 2 && !selected.includes(img)) {
-        img.style.border = "3px solid blue";
+        img.classList.add("selected");
         selected.push(img);
         resetBtn.style.display = "inline-block";
       }
@@ -54,7 +45,7 @@ resetBtn.onclick = function () {
 
 verifyBtn.onclick = function () {
   verifyBtn.style.display = "none";
-  if (selected[0].dataset.value === selected[1].dataset.value) {
+  if (selected[0].className === selected[1].className) {
     message.innerText = "You are a human. Congratulations!";
   } else {
     message.innerText = "We can't verify you as a human. You selected the non-identical tiles.";
@@ -62,67 +53,4 @@ verifyBtn.onclick = function () {
 };
 
 start();
-let allImages = [
-  "https://picsum.photos/id/1015/150/150",
-  "https://picsum.photos/id/1016/150/150",
-  "https://picsum.photos/id/1018/150/150",
-  "https://picsum.photos/id/1020/150/150",
-  "https://picsum.photos/id/1024/150/150"
-];
 
-let imageContainer = document.getElementById("image-container");
-let resetBtn = document.getElementById("reset");
-let verifyBtn = document.getElementById("verify");
-let message = document.getElementById("para");
-
-let selected = [];
-
-function start() {
-  imageContainer.innerHTML = "";
-  message.innerText = "";
-  resetBtn.style.display = "none";
-  verifyBtn.style.display = "none";
-  selected = [];
-
-  let duplicate = allImages[Math.floor(Math.random() * allImages.length)];
-  let sixImages = [...allImages, duplicate];
-  sixImages.sort(() => Math.random() - 0.5);
-
-  for (let i = 0; i < sixImages.length; i++) {
-    let img = document.createElement("img");
-    img.src = sixImages[i];
-    img.dataset.value = sixImages[i];
-    img.classList.add("img" + (i + 1));  
-    imageContainer.appendChild(img);
-  }
-
-  let images = document.querySelectorAll("#image-container img");
-
-  images.forEach(img => {
-    img.addEventListener("click", () => {
-      if (selected.length < 2 && !selected.includes(img)) {
-        img.style.border = "3px solid blue";
-        selected.push(img);
-        resetBtn.style.display = "inline-block";
-      }
-      if (selected.length === 2) {
-        verifyBtn.style.display = "inline-block";
-      }
-    });
-  });
-}
-
-resetBtn.onclick = function () {
-  start();
-};
-
-verifyBtn.onclick = function () {
-  verifyBtn.style.display = "none";
-  if (selected[0].dataset.value === selected[1].dataset.value) {
-    message.innerText = "You are a human. Congratulations!";
-  } else {
-    message.innerText = "We can't verify you as a human. You selected the non-identical tiles.";
-  }
-};
-
-start();
